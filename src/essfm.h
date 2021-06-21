4855b73bdd9406927d26e2090aa0d4fffd51aedd
pachuco <ifail@life.net>
2021-06-21 03:26:00 +0300

    DLL, work on Voice struct.

--- src/essfm.h
+++ src/essfm.h
@@ -26,6 +26,26 @@ void synthMidiQuiet(FmConfig* fmConf);
 ////////////////////////////////////////////////////////////////////////////////////////////
 //AUDDRIVE.DLL
 typedef struct {
-    BYTE data[26];
+    BYTE    flags;
+    BYTE    field_1;
+    USHORT  timer;
+    BYTE    channel;
+    BYTE    field_5;
+    BYTE    field_6;
+    BYTE    field_7;
+    USHORT  field_8;
+    BYTE    field_A;
+    BYTE    field_B;
+    BYTE    field_C;
+    BYTE    field_D;
+    BYTE    field_E;
+    BYTE    field_F;
+    BYTE    field_10;
+    BYTE    field_11[4];
+    BYTE    field_15;
+    BYTE    field_16;
+    BYTE    field_17;
+    BYTE    field_18;
+    BYTE    field_19;
 } Voice;
 static_assert(sizeof(Voice) == 26, "");
