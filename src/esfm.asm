73ba7c8ff0397577b2a263dfb28ea01024272c3d
pachuco <ifail@life.net>
2023-02-23 18:01:04 +0200

    First set of C ports: voice_on, voice_off, !!MidiMessage, !!NATV_CalcBend, MidiCalcFAndB, !!note_on, note_off.

--- src/esfm.asm
+++ src/esfm.asm
@@ -29,23 +29,23 @@ Voice       ends
 
 
 ; export
-public _MidiMessage@4
 public _MidiPitchBend@8
-public _MidiCalcFAndB@8
+extrn _MidiCalcFAndB@8:proc
 public _NATV_CalcVolume@12
 public _NATV_CalcNewVolume@4
 public _NATV_CalcBend@12
-public _voice_off@4
-public _note_off@8
+extrn _voice_off@4:proc
+extrn _note_off@8:proc
 public _note_on@12
 public _setup_voice@20
 public _find_voice@16
 public _steal_voice@4
 public _setup_operator@36
-public _voice_on@4
+extrn _voice_on@4:proc
 public _hold_controller@8
 public _fmreset@0
 public _MidiAllNotesOff@0
+public _MidiMessage@4
 
 ; import
 extrn _fnum:word
@@ -56,7 +56,7 @@ extrn _pmask__MidiPitchBend:byte
 extrn _gBankMem:dword
 extrn _v1:dword
 extrn _v2:dword
-extrn _timer:dword
+extrn _gwTimer:dword
 
 extrn _voice_table:Voice
 extrn _gbVelLevel:byte
@@ -68,7 +68,7 @@ extrn _program_table:byte
 extrn _gbChanVolume:byte
 extrn _gbChanExpr:byte
 extrn _gbChanBendRange:byte
-extrn _byte_6BC09170:byte
+extrn _note_offs:byte
 
 
 extrn _fmwrite231@8:proc
@@ -505,7 +505,7 @@ _MidiPitchBend@8 endp
 
 
 ; __stdcall MidiCalcFAndB(x, x)
-_MidiCalcFAndB@8 proc near      ; CODE XREF: setup_operator(x,x,x,x,x,x,x,x,x)+200p
+ZZZ_MidiCalcFAndB@8 proc near      ; CODE XREF: setup_operator(x,x,x,x,x,x,x,x,x)+200p
                     ; MidiPitchBend(x,x)+8Ap
 
 arg_0       = dword ptr  8
@@ -535,7 +535,7 @@ loc_6BC06636:               ; CODE XREF: MidiCalcFAndB(x,x)+1Aj
         or  eax, ecx
         pop ebp
         retn    8
-_MidiCalcFAndB@8 endp
+ZZZ_MidiCalcFAndB@8 endp
 
 
 
@@ -920,7 +920,7 @@ _NATV_CalcBend@12 endp
 
 
 ; void __stdcall voice_off(signed int a2)
-_voice_off@4    proc near       ; CODE XREF: note_off(x,x)+35p
+ZZZ_voice_off@4    proc near       ; CODE XREF: note_off(x,x)+35p
                     ; find_voice(x,x,x,x)+3Ap ...
 
 a2      = dword ptr  4
@@ -954,14 +954,14 @@ loc_6BC05EE9:               ; CODE XREF: voice_off(x)+13j
                     ; voice_off(x)+28j
         call    _fmwrite231@8   ; fmwrite(x,x)
         mov eax, esi
-        mov cx, word ptr _timer
+        mov cx, word ptr _gwTimer
         imul    eax, 1Ah
-        inc word ptr _timer
+        inc word ptr _gwTimer
         pop esi
         mov byte ptr _voice_table.flags1[eax], 2
         mov _voice_table.timer[eax], cx
         retn    4
-_voice_off@4    endp
+ZZZ_voice_off@4    endp
 
 
 
@@ -1015,7 +1015,7 @@ _voice_off@4    endp
 
 
 ; void __stdcall note_off(unsigned __int8 bChannel, char a2)
-_note_off@8 proc near       ; CODE XREF: MidiAllNotesOff()+Dp
+ZZZ_note_off@8 proc near       ; CODE XREF: MidiAllNotesOff()+Dp
                     ; MidiMessage(x)+297p
 
 bChannel    = byte ptr  4
@@ -1059,7 +1059,7 @@ loc_6BC05F4E:               ; CODE XREF: note_off(x,x)+12j
         pop esi
         pop ebx
         retn    8
-_note_off@8 endp
+ZZZ_note_off@8 endp
 
 
 
@@ -1254,9 +1254,9 @@ loc_6BC06BD3:               ; CODE XREF: note_on(x,x,x)+60j
         movzx   eax, [ebp+channel]
         mov cl, [ebp+arg_8]
         mov _gbVelLevel[eax], cl
-        inc _byte_6BC09170[eax]
-        movzx   ecx, _byte_6BC09170[eax]
-        lea eax, _byte_6BC09170[eax]
+        inc _note_offs[eax]
+        movzx   ecx, _note_offs[eax]
+        lea eax, _note_offs[eax]
         cmp ecx, 100h
         jnz short loc_6BC06BFE
         and byte ptr [eax], 0
@@ -1376,8 +1376,8 @@ a2      = dword ptr  18h
         mov _voice_table.field_10[eax], cl
         mov cl, [ebp+var_1]
         mov _voice_table.flags2[eax], cl
-        mov cx, word ptr _timer
-        inc word ptr _timer
+        mov cx, word ptr _gwTimer
+        inc word ptr _gwTimer
         mov byte ptr _voice_table.flags1[eax], 1
         mov _voice_table.timer[eax], cx
         mov cl, byte ptr [ebp+a6]
@@ -1434,7 +1434,7 @@ loc_6BC05F9F:               ; CODE XREF: find_voice(x,x,x,x)+2Ej
         jnz short loc_6BC05FDB
 
 loc_6BC05FA4:               ; CODE XREF: find_voice(x,x,x,x)+25j
-        mov ax, word ptr _timer
+        mov ax, word ptr _gwTimer
         sub ax, [esi+2]
         cmp ax, bx
         jb  short loc_6BC05FCC
@@ -1477,7 +1477,7 @@ loc_6BC06011:               ; CODE XREF: find_voice(x,x,x,x)+9Dj
         jnz short loc_6BC0605E
 
 loc_6BC0601A:               ; CODE XREF: find_voice(x,x,x,x)+91j
-        mov eax, _timer
+        mov eax, _gwTimer
         sub eax, dword ptr _voice_table.timer+1A0h
         cmp [ebp+a1], 0
         jnz short loc_6BC06049
@@ -1521,7 +1521,7 @@ loc_6BC06088:               ; CODE XREF: find_voice(x,x,x,x)+114j
         jnz short loc_6BC060D4
 
 loc_6BC06091:               ; CODE XREF: find_voice(x,x,x,x)+108j
-        mov eax, _timer
+        mov eax, _gwTimer
         xor ecx, ecx
         sub eax, dword ptr _voice_table.timer+1BAh
         cmp [ebp+a1], ecx
@@ -1597,7 +1597,7 @@ loc_6BC06108:               ; CODE XREF: steal_voice(x)+7Fj
         mov edx, [ebp+var_10]
 
 loc_6BC0610B:               ; CODE XREF: steal_voice(x)+2Aj
-        mov si, word ptr _timer
+        mov si, word ptr _gwTimer
         mov cl, [edx]
         sub si, [edx-2]
         cmp cl, 9
@@ -1964,7 +1964,7 @@ _setup_operator@36 endp
 
 
 ; void __stdcall voice_on(signed int voiceNr)
-_voice_on@4 proc near       ; CODE XREF: note_on(x,x,x)+F3p
+ZZZ_voice_on@4 proc near       ; CODE XREF: note_on(x,x,x)+F3p
                     ; note_on(x,x,x)+189p ...
 
 voiceNr     = dword ptr  4
@@ -1997,7 +1997,7 @@ loc_6BC061A7:               ; CODE XREF: voice_on(x)+11j
                     ; voice_on(x)+26j
         call    _fmwrite231@8   ; fmwrite(x,x)
         retn    4
-_voice_on@4 endp
+ZZZ_voice_on@4 endp
 
 
 
@@ -2120,7 +2120,7 @@ loc_6BC05E74:               ; CODE XREF: fmreset()+AEj
         add eax, 1Ah
         cmp eax, (offset _voice_table.flags1+1D4h)
         jl  short loc_6BC05E74
-        mov word ptr _timer, cx
+        mov word ptr _gwTimer, cx
         pop edi
         retn
 _fmreset@0  endp
--- src/esfm.c
+++ src/esfm.c
@@ -1,11 +1,46 @@
 #include "esfm.h"
 
-extern void __stdcall MidiMessage(DWORD dwData);
-extern void __stdcall fmreset();
-extern void __stdcall MidiAllNotesOff();
+typedef struct {
+    uint8_t    flags1;
+    uint8_t    field_1;
+    uint16_t  timer;
+    uint8_t    channel;
+    uint8_t    bNote;
+    uint8_t    rel_vel;
+    uint8_t    field_7;
+    uint16_t  detune[4];
+    uint8_t    patch_flag;
+    uint8_t    reg5[4];
+    uint8_t    reg1[4];
+    uint8_t    field_19;
+} Voice;
+
+
+
+
+
+
+
+
+
+
+void __stdcall hold_controller(uint8_t channel, uint8_t bVelocity);
+void __stdcall voice_on(int voiceNr);
+void __stdcall voice_off(int voiceNr);
+void __stdcall fmreset();
+uint8_t __stdcall NATV_CalcVolume(uint8_t reg1, uint8_t rel_velocity, uint8_t channel);
+void __stdcall NATV_CalcNewVolume(uint8_t bChannel);
+void __stdcall MidiPitchBend(uint8_t bChannel, uint16_t iBend);
+void __stdcall find_voice(bool patch1617_allowed_voice1, bool patch1617_allowed_voice2, uint8_t bChannel, uint8_t bNote);
+int  __stdcall steal_voice(int patch1617_allowed);
+void __stdcall setup_operator(int offset, int bNote, int bVelocity, uint16_t reg, int fixed_pitch, int rel_velocity, int channel, int operator, int voicenr);
+void __stdcall setup_voice(int voicenr, int offset, int channel, int bNote, int bVelocity);
+void __stdcall MidiMessage(uint32_t dwData);
+
 
 // Only used if ASM source
 #ifndef _ESSFM_H_
+
 uint16_t fnum[] = {
     514, 544, 577, 611, 647, 686, 727, 770, 816, 864, 916, 970
 };
@@ -23,9 +58,9 @@ uint8_t pmask__MidiPitchBend[] = {
 
 uint32_t v1 = 0;
 uint32_t v2 = 0;
-uint32_t timer[2] = {0};
+uint16_t gwTimer = 0;
 
-uint8_t  voice_table[26 * (18+2)] = {0}; //TODO: struct
+Voice    voice_table[18+2]        = {0};
 uint8_t  gbVelLevel[16]           = {0};
 uint8_t  pan_mask[16]             = {0};
 uint8_t  gbChanAtten[16]          = {0};
@@ -35,7 +70,8 @@ uint8_t  program_table[16]        = {0};
 uint8_t  gbChanVolume[16]         = {0};
 uint8_t  gbChanExpr[16]           = {0};
 uint8_t  gbChanBendRange[16]      = {0};
-uint8_t  byte_6BC09170[16+1]      = {0};
+uint8_t  note_offs[16+1]          = {0};
+uint32_t voice1, voice2;
 
 #endif
 
@@ -67,7 +103,7 @@ enum {
     P_INDEX_LO,
     P_INDEX_HI,
 };
-int __stdcall fmwrite231(USHORT index, USHORT data) {
+int __stdcall fmwrite231(uint16_t index, uint16_t data) {
     pWriteCB(0x02, index);
     pWriteCB(0x03, index>>8); pDelayCB();
     pWriteCB(0x01, data); pDelayCB();
@@ -78,7 +114,7 @@ int __stdcall fmwrite231(USHORT index, USHORT data) {
     return 0;
 }
 
-int __stdcall fmwrite21(USHORT index, USHORT data) {
+int __stdcall fmwrite21(uint16_t index, uint16_t data) {
     pWriteCB(0x02, index); pDelayCB();
     pWriteCB(0x01, data); pDelayCB();
     //2 index
@@ -101,7 +137,7 @@ int __stdcall fmwrite21(USHORT index, USHORT data) {
 
 
 
-void esfm_init(BYTE* pBank, FunWriteCB pfWr, FunDelayCB pfDly) {
+void esfm_init(uint8_t* pBank, FunWriteCB pfWr, FunDelayCB pfDly) {
     esfm_setBank(pBank);
     pWriteCB = pfWr;
     pDelayCB = pfDly;
@@ -110,7 +146,7 @@ void esfm_init(BYTE* pBank, FunWriteCB pfWr, FunDelayCB pfDly) {
     esfm_resetFM();
 }
 
-void esfm_setBank(BYTE* pBank) {
+void esfm_setBank(uint8_t* pBank) {
     gBankMem = pBank;
 }
 
@@ -142,6 +178,318 @@ void esfm_shutdownDevice() {
     pWriteCB(0x07, 98); pDelayCB();
 }
 
-void esfm_midiShort(DWORD dwData) {
+void esfm_midiShort(uint32_t dwData) {
     MidiMessage(dwData);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void __stdcall voice_on(int voiceNr)
+{
+    if ( voiceNr >= 16 )
+    {
+        if ( voiceNr == 16 )
+        {
+            fmwrite231(0x250, 1);
+            fmwrite231(0x251, 1);
+        }
+        else
+        {
+            fmwrite231(0x252, 1);
+            fmwrite231(0x253, 1);
+        }
+    }
+    else 
+    {
+        fmwrite231((uint16_t)voiceNr + 0x240, 1);
+    }
+}
+
+void __stdcall voice_off(int voiceNr)
+{
+    if ( voiceNr >= 16 )
+    {
+        if ( voiceNr == 16 )
+        {
+          fmwrite231(0x250, 0);
+          fmwrite231(0x251, 0);
+        }
+        else
+        {
+          fmwrite231(0x252, 0);
+          fmwrite231(0x253, 0);
+        }
+    }
+    else
+    {
+        fmwrite231((uint16_t)voiceNr + 0x240, 0);
+    }
+    voice_table[voiceNr].flags1 = 2;
+    voice_table[voiceNr].timer = (uint16_t)gwTimer;
+    gwTimer++;
+}
+
+/*
+void __stdcall MidiMessage(uint32_t dwData) 
+{
+    uint8_t    bMsgType, bChannel, bVelocity, bNote;
+    int     i;
+
+    // D1("\nMidiMessage");
+    bMsgType =    (uint8_t)((dwData & 0x000000F0));
+    bChannel =    (uint8_t)((dwData & 0x0000000F) >> 0);
+    bNote =       (uint8_t)((dwData & 0x00007F00) >> 8);
+    bVelocity =   (uint8_t)((dwData & 0x007F0000) >> 16);
+    
+    switch (bMsgType) 
+    {
+        case 0x90:      // turn key on, or key off if volume == 0
+            if (bVelocity)
+            {
+                note_on((uint8_t)dwData, bNote, bVelocity); 
+            }
+            else 
+            {
+                note_off((uint8_t)dwData, bNote);
+            }
+            break;
+        case 0x80:      // turn key off
+            note_off((uint8_t)dwData, bNote);
+            break;
+        case 0xb0:      // change control
+            switch (bNote)
+            {
+                case 6:
+                    if ( (hold_table[bChannel] & 6) == 6 )
+                        gbChanBendRange[bChannel] = bVelocity;
+                    break;
+                case 7:
+                    gbChanAtten[bChannel] = gbVelocityAtten[bVelocity >> 1];
+                    gbChanVolume[bChannel] = bVelocity;
+                    NATV_CalcNewVolume((uint8_t)dwData);
+                    break;
+                case 8:
+                case 10:
+                    if ( bVelocity <= 80 )
+                    {
+                      if ( bVelocity >= 48 )
+                        pan_mask[bChannel] = 48;
+                      else
+                        pan_mask[bChannel] = 16;
+                    }
+                    else
+                    {
+                      pan_mask[bChannel] = 32;
+                    }
+                    break;
+                case 11:
+                    gbChanExpr[bChannel] = bVelocity;
+                    NATV_CalcNewVolume((uint8_t)dwData);
+                    break;
+                case 64:
+                    hold_controller(bChannel, bVelocity);
+                    break;
+                case 98:
+                    hold_table[bChannel] &= ~2;
+                    break;
+                case 99:
+                    hold_table[bChannel] &= ~4;                    
+                    break;
+                case 100:
+                    if ( bVelocity == 0 )
+                    {
+                        hold_table[bChannel] |= 2;
+                    }
+                    else
+                    {
+                        hold_table[bChannel] &= ~2;
+                    }
+                    break;
+                case 101:
+                    if ( bVelocity == 0 )
+                    {
+                        hold_table[bChannel] |= 4;
+                    }
+                    else
+                    {
+                        hold_table[bChannel] &= ~4;
+                    }
+                    break;
+                case 120:
+                case 124:
+                case 125:
+                    for (i = 0; i < sizeof(voice_table)/sizeof(voice_table[0]); i++)
+                    {
+                        if ((voice_table[i].flags1 & 1) && voice_table[i].channel == bChannel)
+                            voice_off(i);
+                    }
+                    break;
+                case 121:
+                    if (hold_table[bChannel] & 1)
+                    {
+                        for (i = 0; i < sizeof(voice_table)/sizeof(voice_table[0]); i++)
+                        {
+                            if ((voice_table[i].flags1 & 1) && voice_table[i].channel == bChannel && (voice_table[i].flags1 & 4))
+                                voice_off(i);
+                        }
+                    }
+                    hold_table[bChannel] &= ~1u;
+                    gbChanVolume[bChannel] = 100;
+                    gbChanExpr[bChannel] = 127;
+                    giBend[bChannel] = 0x2000;
+                    pan_mask[bChannel] = 48;
+                    gbChanBendRange[bChannel] = 2;
+                    break;
+                case 123:
+                case 126:
+                case 127:
+                    for (i = 0; i < sizeof(voice_table)/sizeof(voice_table[0]); i++)
+                    {
+                        if ((voice_table[i].flags1 & 1) && voice_table[i].channel == bChannel && (voice_table[i].flags1 & 4) == 0)
+                            voice_off(i);
+                    }
+                    break;
+            }
+            break;
+        case 0xc0:
+            program_table[bChannel] = bNote;
+            break;
+        case 0xe0:      // pitch bend
+            MidiPitchBend((uint8_t)dwData, bNote | (bVelocity << 7));
+            break;
+    }
+}
+*/
+
+/*int16_t __stdcall NATV_CalcBend(uint16_t detune, int16_t iBend, uint16_t iBendRange)
+{
+static uint16_t NATV_table1[64] = {
+    1024, 1025, 1026, 1027, 1028, 1029, 1030, 1030, 1031, 1032,
+    1033, 1034, 1035, 1036, 1037, 1038, 1039, 1040, 1041, 1042,
+    1043, 1044, 1045, 1045, 1046, 1047, 1048, 1049, 1050, 1051,
+    1052, 1053, 1054, 1055, 1056, 1057, 1058, 1059, 1060, 1061,
+    1062, 1063, 1064, 1065, 1065, 1066, 1067, 1068, 1069, 1070,
+    1071, 1072, 1073, 1074, 1075, 1076, 1077, 1078, 1079, 1080,
+    1081, 1082, 1083, 1084,
+};
+static uint16_t NATV_table2[49] = {
+    256,  271,  287,  304,  323,  342,  362,  384,  406,  431,
+    456,  483,  512,  542,  575,  609,  645,  683,  724,  767,
+    813,  861,  912,  967,  1024, 1085, 1149, 1218, 1290, 1367,
+    1448, 1534, 1625, 1722, 1825, 1933, 2048, 2170, 2299, 2435,
+    2580, 2734, 2896, 3069, 3251, 3444, 3649, 3866, 4096,
+};
+    //!WARN iBend is int16 in OPL midi driver sample
+    if ( iBend == 0x2000 ) 
+        return detune & 0xFF;
+    else 
+	{
+        int v5;
+        if ( iBend >= 0x3F80 ) iBend = 0x4000;
+        v5 = (iBendRange * (iBend - 0x2000) >> 5) + 0x1800;
+        return (detune * (uint16_t)((NATV_table1[(v5>>2)&0x3F] * NATV_table2[v5>>8]) >> 10) + 512) >> 10;
+    }
+}*/
+
+uint32_t __stdcall MidiCalcFAndB(uint32_t bend, uint8_t block) {
+    while (bend >= 1024) {
+        bend >>= 1;
+        block++;
+    }
+    if ( block > 7 ) block = 7;
+    return bend  | (block << 10);
+}
+
+/*
+void __stdcall note_on(uint8_t channel, uint8_t bNote, uint8_t bVelocity)
+{
+    int patch;
+    int offset;
+    uint8_t flags_voice1;
+    int fixed_pitch;
+
+    if ( channel == 9 )
+        patch = bNote + 128;
+    else
+        patch = program_table[channel];
+    offset = gBankMem[2 * patch] + (gBankMem[2 * patch + 1] << 8);
+    if ( offset )
+    {
+        flags_voice1 = gBankMem[offset];
+        fixed_pitch = (flags_voice1 >> 1) & 3;
+        switch (fixed_pitch)
+        {
+        case 0:
+            find_voice(flags_voice1 & 1, 0, channel, bNote);
+            if ( voice1 == 255 ) voice1 = steal_voice(gBankMem[offset] & 1);
+            setup_voice(voice1, offset, channel, bNote, bVelocity);
+            voice_on(voice1);
+            break;
+        case 1:
+            find_voice(flags_voice1 & 1, gBankMem[offset + 36] & 1, channel, bNote);
+            if (voice1 == 255) voice1 = steal_voice(gBankMem[offset] & 1);
+            setup_voice(voice1, offset, channel, bNote, bVelocity);
+            if (voice2 != 255)
+            {
+                setup_voice(voice2, offset + 36, channel, bNote, bVelocity);
+                voice_table[voice2].flags1 |= 8u;
+                voice_on(voice2);
+            }
+            voice_on(voice1);
+            break;
+        case 2:
+            find_voice(flags_voice1 & 1, gBankMem[offset + 36] & 1, channel, bNote);
+            if ( voice1 == 255 )
+            voice1 = steal_voice(gBankMem[offset] & 1);
+            if ( voice2 == 255 )
+            voice2 = steal_voice(gBankMem[offset + 36] & 1);
+            setup_voice(voice1, offset, channel, bNote, bVelocity);
+            setup_voice(voice2, offset + 36, channel, bNote, bVelocity);
+            voice_on(voice1);
+            voice_on(voice2);
+            break;
+        }
+        gbVelLevel[channel] = bVelocity;
+        if (note_offs[channel] == 255) note_offs[channel]=0; else note_offs[channel]++;
+    }
+}
+*/
+
+void __stdcall note_off(uint8_t bChannel, uint8_t bNote) {
+    int i;
+    
+    for (i=0; i<18; i++) {
+		Voice *voice = &voice_table[i];
+        if ((voice->flags1 & 1) && voice->channel == bChannel && voice->bNote == bNote) {
+            if ( hold_table[bChannel] & 1 ) {
+                voice->flags1 |= 4;
+            } else {
+                voice_off(i);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
